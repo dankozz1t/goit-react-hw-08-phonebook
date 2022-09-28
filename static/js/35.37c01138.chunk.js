d742c43511061e5edd6cc563bf9d8f7be114cdab
dankozz1t <AlexDankoXXL@gmail.com>
2022-09-28 13:52:59 +0000

    Deploying to gh-pages from @ dankozz1t/goit-react-hw-08-phonebook@230a4fb9d971a703add2daa477310ce3ef538c9e 🚀

---
+++ static/js/35.37c01138.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[35],{35:function(e,t,r){r.r(t),r.d(t,{default:function(){return u}});r(791);var n=r(184),u=function(){return(0,n.jsx)("h2",{children:"HomePage"})}}}]);
+//# sourceMappingURL=35.37c01138.chunk.js.map
