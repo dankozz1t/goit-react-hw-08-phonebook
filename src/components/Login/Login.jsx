87641d2a23e92b4e4e71d12ce18aa54ef71594d1
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2022-10-02 11:39:06 +0300

    fix pattern for email

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -64,7 +64,7 @@ const Login = () => {
             placeholder={lang.login_placeholderInputEmail}
             type="email"
             name="email"
-            pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
+            pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$"
             title={lang.login_titleInputEmail}
             value={email}
             onChange={handleInputChange}
--- src/components/Singup/Singup.jsx
+++ src/components/Singup/Singup.jsx
@@ -85,7 +85,7 @@ const Singup = () => {
             placeholder={lang.singup_placeholderInputEmail}
             type="email"
             name="email"
-            pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
+            pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$"
             title={lang.singup_titleInputEmail}
             value={email}
             onChange={handleInputChange}
